467a98a14b58d5245ed9bff5cfabe742cc379bf5
tkawa <tkawa@4bit.net>
2011-12-04 14:41:02 +0900

    added sign in with github omniauth

--- app.rb
+++ app.rb
@@ -1,17 +1,30 @@
 # coding: utf-8
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'omniauth'
 
 ENV['RACK_ENV'] ||= 'development'
 configure do
   Mongoid.load!('./mongoid.yml')
 end
 
+enable :sessions
+
 helpers do
   include Rack::Utils
   alias_method :h, :escape_html
 end
 
+use OmniAuth::Builder do
+  #provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github, 'a00008e68e9e9fcdbba5', 'ff1e60120c214d14533187a856ff73adf21feeff'
+end
+
 get '/' do
+  erb :index
+end
+
+get '/auth/github/callback' do
+  @auth = request.env['omniauth.auth']
   erb :index
 end
