8b95c083c9310759ceacdbd2238e8711a7fbf4df
u <ishihara.yu@gmail.com>
2011-12-04 16:57:24 +0900

    add
    Signed-off-by: u <ishihara.yu@gmail.com>

--- public/badge.js
+++ public/badge.js
@@ -1,8 +1,7 @@
 
 
-var url_badge = './images/badge-';
-var url_site = '';
-var element_badge = 'twitterFollowBadge';
+var url_badge = './images/';
+var element_badge = 'folkmebadge';
 
 var o = {};
 o.defaultTop=100;
@@ -15,8 +14,9 @@ o.showbadge = function(){
 		document.body.removeChild(document.getElementById(element_badge));
 	}
 	
-	o.tabStyleCode='-moz-box-shadow:0px 1px 3px rgba(000,000,000,0.5),inset 0px 0px 1px rgba(255,255,255,0.6);-webkit-box-shadow:0px 1px 3px rgba(000,000,000,0.5),inset 0px 0px 1px rgba(255,255,255,0.6);z-index:9999;position:absolute;'+'top:'+o.top+'px;'+'width:31px;'+'height:119px;'+'z-index:999;'+'cursor:pointer;'+'background:'+o.color+' url(/images/badge.png);'+'background-repeat:no-repeat;';
-	o.aboutStyleCode='position:fixed;'+'top:'+(parseInt(o.top)+107)+'px;'+'width:10px;'+'height:11px;'+'z-index:9876;'+'cursor:pointer;'+'background:url(./images/about.png);'+'background-repeat:no-repeat;';
+	o.tabStyleCode='-moz-box-shadow:0px 1px 3px rgba(000,000,000,0.5),inset 0px 0px 1px rgba(255,255,255,0.6);-webkit-box-shadow:0px 1px 3px rgba(000,000,000,0.5),inset 0px 0px 1px rgba(255,255,255,0.6);position:fixed;'+'top:'+o.top+'px;'+'width:31px;'+'height:119px;'+'z-index:9998;'+'cursor:pointer;'+'background:'+o.color+' url(/images/badge.png);'+'background-repeat:no-repeat;';
+	
+	o.aboutStyleCode='position:fixed;'+'top:'+(parseInt(o.top)+106)+'px;'+'margin-top:6px;margin-left:9px;margin-right:9px;width:13px;'+'height:15px;'+'z-index:9999;'+'cursor:pointer;'+'background: url(/images/about.png);'+'background-repeat:no-repeat;';
 	if(o.side=='l'){
 		o.tabStyleCode+='left:0; background-position:right top;';
 		o.aboutStyleCode+='left:0;';
@@ -28,14 +28,18 @@ o.showbadge = function(){
 	elements=document.createElement('div');
 	elements.setAttribute('id',element_badge);
 	document.body.appendChild(elements);
-	elements.innerHTML='<div id="'+element_badge+'" style="'+o.tabStyleCode+'"></div><div id="tfbAbout" style="'+o.aboutStyleCode+'"></div>'+'<style>#tfbAbout{visibility:hidden;} #twitterFollowBadge:hover #tfbAbout{visibility:visible;}</style>';
+	elements.innerHTML='<div id="'+element_badge+'" style="'+o.tabStyleCode+'"></div><div id="fmAbout" style="'+o.aboutStyleCode+'"></div>'+'<style>#fmAbout{visibility:hidden;} #folkmebadge:hover #fmAbout{visibility:visible;}</style>';
 	document.getElementById(element_badge).onclick=function(){
 		window.open('http://github.com/'+o.account);
 	}
-	document.getElementById('tfbAbout').onclick=function(){
-		window.open('http://xxxxxxxxxxxxxxxxxx/')
+	document.getElementById('fmAbout').onclick=function(){
+		window.open('https://forkmebadge.heroku.com/')
 	}
 	
+	$('#folkmebadge').hover(
+		function() { $(this).fadeTo(10, 0.2); $(this).fadeTo(300, 1); },
+		function() {}
+	);
 	
 }
 
